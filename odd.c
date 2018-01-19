22477f859a0a0cec98c3dceb33ae3e3f7f172d26
Nirmalasumi1 <30829249+Nirmalasumi1@users.noreply.github.com>
2018-01-19 15:56:16 +0530

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int i,m,n;
+printf("Enter the range of m and n:");
+scanf("%d %d",&m &n);
+for(i=m;i<=n;i++)
+{
+if(i%2==1)
+{
+printf("%d" The odd numbers are:);
+}
+}
+}
