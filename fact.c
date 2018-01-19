11c985ec5c91070f386710fe32df3f5f5bb8e1e7
Nirmalasumi1 <30829249+Nirmalasumi1@users.noreply.github.com>
2018-01-19 16:51:57 +0530

    Create fact.c

---
+++ fact.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int i,fact=1,num;
+printf("Enter the number:");
+scanf("%d",&num);
+if(num<=0)
+fact=1;
+else
+{
+for(i=1;i<=num;i++)
+{
+fact=fact*i;
+}
+}
+printf("Factorial of %d=%d\n",num,fact);
+}
