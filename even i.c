9292e0267b9618f24aa4c86003b3c2538e2696cf
Nirmalasumi1 <30829249+Nirmalasumi1@users.noreply.github.com>
2018-01-19 16:16:42 +0530

    Create even i.c

---
+++ even i.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int i,m,n;
+printf("Enter the range of m and n:");
+scanf("%d%d",&m,&n);
+for(i=m;i<=n;i++)
+{
+if(i%2==0)
+{
+printf("The even numbers are:");
+}
+}
+return 0;
+}
