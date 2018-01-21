418c81a2ad282bee07bd665e01c95d4fc597ab00
Nirmalasumi1 <30829249+Nirmalasumi1@users.noreply.github.com>
2018-01-21 10:16:34 +0530

    Update armstrong.c

--- armstrong.c
+++ armstrong.c
@@ -7,13 +7,15 @@ scanf("%d",&num);
 while(num!=0)
 {
 r=num%10;
-result=r*r*r;
+result+=r*r*r;
 num=num/10;
 }
 if(result==num)
 {
 printf("It is an armstrong number:");
+}
 else
+{
 printf("It is not an armstrong number:");
 }
   return 0;
