90e6815aa755cc9b14978524f33125d3f47c37e1
Nirmalasumi1 <30829249+Nirmalasumi1@users.noreply.github.com>
2018-01-21 10:05:03 +0530

    Create new file

---
+++ armstrong.c
@@ -0,0 +1,19 @@
+#include<stdio.h
+void main()
+{
+int num,r,result=0;
+printf("Enter the number:");
+scanf("%d",&num);
+while(num!=0)
+{
+r=num%10;
+result=r*r*r;
+num=num/10;
+}
+if(result==num)
+{
+printf("It is an armstrong number:");
+else
+printf("It is not an armstrong number:");
+}
+}
