840f4eab58616ea6a03c743709d5489c3cab8d3b
Nirmalasumi1 <30829249+Nirmalasumi1@users.noreply.github.com>
2018-01-21 10:29:08 +0530

    Create armsi.c

---
+++ armsi.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+int m,n,i,r,result=0;
+printf("Enter the range of m and n:");
+scanf("%d%d",&m,&n);
+for(i=m;i<=n;i++)
+{
+r=i%10;
+result=result+(r*r*r);
+i=i/10;}
+}
+if(result==i)
+{
+printf("It is an armstrong number:");
+}
+else
+{
+printf("It is not an armstrong number:");
+}
+return 0;
+}
