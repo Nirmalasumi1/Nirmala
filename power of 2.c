83ca73cd88308a7a1f7a7086cae2e22c27acee2c
Nirmalasumi1 <30829249+Nirmalasumi1@users.noreply.github.com>
2018-01-23 16:38:46 +0530

    Update power of 2.c

--- power of 2.c
+++ power of 2.c
@@ -6,13 +6,18 @@ printf("Enter a number:");
 scanf("%d",&num);
 while(num==0)
 {
-return 1;}
+return 1;
+}
 while(num>1)
 {
 if(num%2==0)
+{
 printf("It is power of 2:");
+}
 else
+{
 printf("It is not power of 2:");
 }
 return 0;
+}
 }
