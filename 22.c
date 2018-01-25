bbdd1d9bc0d8d6456ed0b47db2103cc896191589
Nirmalasumi1 <30829249+Nirmalasumi1@users.noreply.github.com>
2018-01-25 16:25:23 +0530

    Create 22.c

---
+++ 22.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int n,a[25],i,max;
+printf("Enter how many elements:");
+scanf("%d",&n);
+printf("Enter the elements:");
+for(i=0;i<n;i++)
+scanf("%d",&a[i]);
+for(i=1;i<n;i++)
+{
+if(max<a[i])
+{
+printf("%d",The maximum element is:");
+}
+}return 0;
+}
