e5b6c52846877c4cb05e78a7167bb6b41fcb86b0
Nirmalasumi1 <30829249+Nirmalasumi1@users.noreply.github.com>
2018-01-25 16:37:46 +0530

    Update 22.c

--- 22.c
+++ 22.c
@@ -7,11 +7,12 @@ scanf("%d",&n);
 printf("Enter the elements:");
 for(i=0;i<n;i++)
 scanf("%d",&a[i]);
+ max=a[0];
 for(i=1;i<n;i++)
 {
 if(max<a[i])
 {
-  max=a[i];
+ max=a[i];
 printf("The max element is %d",max);
 }
 }return 0;
