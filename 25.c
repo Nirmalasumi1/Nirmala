249aa28294ef54cef8070c894f126dec6c67d98c
Nirmalasumi1 <30829249+Nirmalasumi1@users.noreply.github.com>
2018-02-01 10:45:47 +0000

    Create 25.c

---
+++ 25.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int a[size],size,n,i,median;
+printf("Enter the size of an array:");
+scanf("%d",&size);
+printf("Enter how many elements:");
+scanf("%d",&n);
+printf("Enter the elements of an array:");
+scanf("%d"a[i]);
+while(n!=0)
+{
+median=n+1/2;
+printf("The median element is:%d");
+}
+return 0;
+}
