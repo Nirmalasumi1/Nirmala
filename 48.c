51cf91f2de234b52cd6fdb80c6e8de77b065ba5b
Nirmalasumi1 <30829249+Nirmalasumi1@users.noreply.github.com>
2018-02-02 09:25:43 +0000

    Create 48.c

---
+++ 48.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int n,a[i],sum=0,avg;
+printf("Enter the value of n:");
+scanf("%d",&n);
+printf("The numbers are:");
+scanf("%d",&a[i]);
+for(i=0;i<n;i++)
+{
+sum=sum+a[i];
+}
+avg=sum/n;
+printf("The average is:%d",avg);
+return 0;
+}
