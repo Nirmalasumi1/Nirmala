fd01189c23748a5916df209b25f1bd004efb7b66
Nirmalasumi1 <30829249+Nirmalasumi1@users.noreply.github.com>
2018-02-06 08:49:49 +0000

    Update 30.c

--- 30.c
+++ 30.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-int hrs1,,hrs2,mins1,mins2,a,b;
+int hrs1,hrs2,mins1,mins2,a,b;
 printf("Enter the hrs1 and mins1:");
 scanf("%d%d",&hrs,&mins);
 printf("Enter the hrs2 and mins2:");
