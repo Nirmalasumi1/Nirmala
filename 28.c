9485dea4b9f088bca1180506a656fef9c8ed53a4
Nirmalasumi1 <30829249+Nirmalasumi1@users.noreply.github.com>
2018-02-06 08:57:51 +0000

    Update 28.c

--- 28.c
+++ 28.c
@@ -1,12 +1,15 @@
 #include<stdio.h>
 int main()
 {
-int a[],n,i;
+int a[25],n,i;
 printf("Enter how many elemens:");
 scanf("%d",&n);
 printf("Enter the array elements:");
 for(i=0;i<n;i++)
 scanf("%d",&a[i]);
-printf("%d %d",a[i],i);
+for(i=0;i<n;i++)
+{
+printf("\n%d %d\n",a[i],i);
+}
 return 0;
 }
