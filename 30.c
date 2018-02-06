f0e8ab046709a23d6abcccc3346614591a00cb72
Nirmalasumi1 <30829249+Nirmalasumi1@users.noreply.github.com>
2018-02-06 08:44:32 +0000

    Create 30.c

---
+++ 30.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int hrs,mins,a,b,sub;
+printf("Enter the hrs and mins:");
+scanf("%d%d",&hrs,&mins);
+printf("Enter the hrs and mins:");
+scanf("%d%d",&hrs,&mins);
+a=hrs-hrs;
+b=mins-mins;
+printf("%d %d",a,b);
+return 0;
+}
+
