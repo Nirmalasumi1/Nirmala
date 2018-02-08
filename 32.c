0dfbd5538c8bc6a31cea016ba015c92e8a8db873
Nirmalasumi1 <30829249+Nirmalasumi1@users.noreply.github.com>
2018-02-08 08:45:12 +0000

    Update 32.c

--- 32.c
+++ 32.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
+#include<string.h>
 int main()
 {
 char s1[100];
 int count=0,i;
 printf("Enter the string:");
-scanf("%s",&s1);
-for(i=0;s1[i]!=0;i++)
+scanf("%[\n]s",&s1);
+for(i=0;s1[i] !=0;i++)
 {
-if(s1[i]=='')
+if(s1[i]==' ')
 count++;
 }
 printf("The number of words in a given string is %d",count+1);
