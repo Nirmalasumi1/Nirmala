cb76119c239142389d1c0cb1b7f6898973f60a72
Nirmalasumi1 <30829249+Nirmalasumi1@users.noreply.github.com>
2018-02-09 08:49:32 +0000

    Update 35.c

--- 35.c
+++ 35.c
@@ -1,13 +1,16 @@
 #include<stdio.h>
 int main()
 {
-char s[100],count=0;
+char s[100],i,count=0;
 printf("Enter the string:");
 scanf("%s",&s);
+for(i=0;s[i]!='\0',i++)
+{
 if(s>='0'&& s<='9')
 {
 count++;
-printf("%d",count+1);
 }
+}
+printf("%d",count+1);
 return 0;
 }
