2d0479ce149eb2ae3acea0529be2dddb75322304
Nirmalasumi1 <30829249+Nirmalasumi1@users.noreply.github.com>
2018-02-13 08:39:12 +0000

    Create 59.c

---
+++ 59.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int a[10],i,max;
+printf("Enter the numbers:");
+for(i=0;i<a[i];i++)
+scanf("%d",&a[i]);
+for(i=0;i<a[i];i++)
+{
+max=a[0];
+if(a[i]>max)
+{
+max=a[i];
+}
+printf("The maximum number is %d",max);
+}
+return 0;
+}
