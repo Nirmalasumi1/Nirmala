3f43cc05bfdcf27d4c1d35c21e0bd6bae3a7c769
Nirmalasumi1 <30829249+Nirmalasumi1@users.noreply.github.com>
2018-02-13 09:04:30 +0000

    Create 58.c

---
+++ 58.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int m,n;
+printf("Enter the two numbers:");
+scanf("%d%d",&m,&n);
+printf("Before swapping m=%d n=%d",m,n);
+m=m^n;
+n=m^n;
+m=m^n;
+printf("After swapping m=%d n=%d",m,n);
+return 0;
+}
