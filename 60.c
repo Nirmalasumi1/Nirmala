01dca0f66a95a87507f410d5422b038ef00ee771
Nirmalasumi1 <30829249+Nirmalasumi1@users.noreply.github.com>
2018-02-13 08:30:15 +0000

    Update 60.c

--- 60.c
+++ 60.c
@@ -1,15 +1,22 @@
 #include<stdio.h>
 int main()
 {
-int x=0,y=1,z,n;
+int x=0,y=1,z,n,i;
 printf("Enter the number:");
 scanf("%d",&n);
-while(n!=0)
+for(i=1;i<=n;i++)
 {
+ if(n<=1)
+ {
+   z=i;
+ }
+ else
+ {
 z=x+y;
 x=y;
 y=z;
+ }
 }
-printf("The fibonacci series is %d",z);
+printf("The fibonacci series is %d\n",z);
 return 0;
 }
