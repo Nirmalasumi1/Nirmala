e0b37ebf214322ce4c953c3340fcb9a7ff9d51f5
Nirmalasumi1 <30829249+Nirmalasumi1@users.noreply.github.com>
2018-02-27 16:50:01 +0530

    Update 66.c

--- 66.c
+++ 66.c
@@ -4,7 +4,7 @@ int main()
 int n,i,n1;
 printf("Enter the number:");
 scanf("%d",&n);
-for(i=0;i<n;i++);
+for(i=1;i<n;i++);
 {
 if(n%i==0)
 {
@@ -14,10 +14,8 @@ n1++;
 if(n1==2)
 {
 printf("It is prime");
-}
 else
-{
 printf("It is not prime");
-}
 return 0;
+}
 }
