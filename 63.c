ce715f65a4ca8c77a0df8cb2709824c3804ff19e
Nirmalasumi1 <30829249+Nirmalasumi1@users.noreply.github.com>
2018-02-27 16:39:18 +0530

    Update 63.c

--- 63.c
+++ 63.c
@@ -2,9 +2,9 @@
 int main()
 {
 char s[50];
-int i,count;
+int i,count=0;
 printf("Enter the string:");
-scanf("%s",&s);
+scanf("%[^\n]s",&s);
 for(i=0;s[i]!='\0';i++)
 {
 if(s[i]==' ')
@@ -12,6 +12,6 @@ if(s[i]==' ')
 count++;
 }
 }
-printf("%d",count);
+printf("%d",count+1);
 return 0;
 }
