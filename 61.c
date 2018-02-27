b89527e9d3fead3a5293aebb30ec66786478090f
Nirmalasumi1 <30829249+Nirmalasumi1@users.noreply.github.com>
2018-02-27 15:56:42 +0530

    Update 61.c

--- 61.c
+++ 61.c
@@ -3,14 +3,14 @@
 int main()
 {
 char s[50];
-int i,n;
-printf("Enter the number:");
-scanf("%d",&n);  
+int i,n;  
 printf("Enter the string:");
 scanf("%s",&s);
+printf("Enter the number:");
+scanf("%d",&n);  
 for(i=0;i<n;i++)
 {
-printf("%c",i);
+printf("%c",s[i]);
 }
 return 0;
 }
