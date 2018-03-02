84301e6aaf9cf1491dd6072cdea0f697260f2a2d
Nirmalasumi1 <30829249+Nirmalasumi1@users.noreply.github.com>
2018-03-02 16:07:34 +0530

    Update 62.c

--- 62.c
+++ 62.c
@@ -1,20 +1,29 @@
 #include<stdio.h>
+#include<string.h>
 int main()
 {
-int n[50],i;
+char n[50];
+int i,l,flag=0;
+l=strlen(n);
 printf("Enter the number:");
-scanf("%d",&n);
-for(i=0;i<n;i++)
+scanf("%s",&n);
+for(i=0;i<l;i++)
 {
-scanf("%d",&n[i]);
-}
-if(n[i]=='0' || n[i]=='1')
+if(n[i]=='1' || n[i]=='0')
 {
-printf("yes");
+  flag=1;
 }
 else
 {
-printf("no");
+  flag=0;
+break;
 }
-return 0;
+}
+if(flag==1)
+{
+  printf("yes");
+}
+else
+{
+  printf("no");
 }
