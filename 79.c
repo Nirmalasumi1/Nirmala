69808e369d5d15eda0b24116db03a4445ff56d58
Nirmalasumi1 <30829249+Nirmalasumi1@users.noreply.github.com>
2018-03-08 15:51:51 +0530

    Create 79.c

---
+++ 79.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+printf("Enter the two numbers:");
+scanf("%d%d",&a,&b);
+if(a==b)
+{
+printf("It is perfect square");
+}
+else
+{
+printf("It is not perfect square");
+}
+return 0;
+}
