e23bd32e034680e51a5919fd2c741cabd9fe8c3e
Nirmalasumi1 <30829249+Nirmalasumi1@users.noreply.github.com>
2018-03-09 16:04:40 +0530

    Create 71.c

---
+++ 71.c
@@ -0,0 +1,27 @@
+#inlcude<stdio.h>
+#include<string.h>
+int main()
+{
+char str[100];
+int len,i,flag=0;
+printf("Enter the string:");
+scanf("%s",&str);
+len=strlen(s);
+for(i=0;i<len;i++)
+{
+if(str[i]==str[len-i-1])
+{
+flag=1;
+break;
+}
+}
+if(flag==1)
+{
+printf("It is palindrome");
+}
+else
+{
+printf("It is not palindrome");
+}
+return 0;
+}
