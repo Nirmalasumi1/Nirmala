f06fb6456a9aff52b9a54b52f5f29ca2327b340b
Nirmalasumi1 <30829249+Nirmalasumi1@users.noreply.github.com>
2018-03-09 16:57:52 +0530

    Update 70.c

--- 70.c
+++ 70.c
@@ -2,6 +2,7 @@
 int main()
 {
 int num,c=0,n=1,p=2,i;
+printf("Enter the number:"); 
 scanf("%d",&num);
 while(num!=1)
 {
@@ -12,6 +13,6 @@ for(i=0;i<c+1;i++)
 {
 n=n*p;
 }
-printf("The nearest greater power of 2 is:%d",x);
+printf("The nearest greater power of 2 is:%d",n);
 return 0;
 }
